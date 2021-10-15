122cb0546b000de51ffb0547e1dbfedea38d7b6b
Christian Klinger <ck@novareto.de>
2021-10-15 08:46:39 +0200

    chagnes

--- reha/example/person.py
+++ reha/example/person.py
@@ -1,8 +1,21 @@
-
-from uvcreha.contents import documents_store
 from uvcreha.contents import documents_store
 from dataclasses import dataclass, field
 from dataclasses_jsonschema import JsonSchemaMixin
+from uvcreha.browser.document import (
+    DefaultDocumentEditForm,
+    DocumentEdit,
+    DefaultDocumentPDF,
+    PDFView,
+)
+from reha.example import TEMPLATES
+import io
+
+
+from borb.pdf.canvas.layout.page_layout.multi_column_layout import SingleColumnLayout
+from borb.pdf.canvas.layout.text.paragraph import Paragraph
+from borb.pdf.document import Document
+from borb.pdf.page.page import Page
+from borb.pdf.pdf import PDF
 
 
 @dataclass
@@ -16,3 +29,24 @@ class Person(JsonSchemaMixin):
 
 
 documents_store.add("person", Person.json_schema())
+
+
+@DocumentEdit.component("person.1.0")
+class CTDocumentEditForm(DefaultDocumentEditForm):
+    template = TEMPLATES["person_edit.pt"]
+
+
+@PDFView.component("person.1.0")
+class PersonPdf(DefaultDocumentPDF):
+
+    def generate_pdf(self, context):
+        pdf = Document()
+        page = Page()
+        pdf.append_page(page)
+        layout = SingleColumnLayout(page)
+        layout.add(Paragraph("Hello World!"))
+        layout.add(Paragraph(context.item))
+        stream = io.BytesIO()
+        PDF.dumps(stream, pdf)
+        stream.seek(0)
+        return stream
